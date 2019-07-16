9cfa9297170da66d3ead748e88a5e655dceb380a
John Askew <bucbowie@gmail.com>
2019-07-16 12:51:07 -0400

    fin. knn

--- 030-test.py
+++ 030-test.py
@@ -9,64 +9,25 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from askew_utils import DF_Magic as dfm
-#-------------------------------------#
-def global_variables_function():
-#-------------------------------------#
-    global refresh_variables
-    global return_njobs
-    global return_xor
-    global return_nneighbors
-    global return_nsplits
-    global return_nestimators
-    global return_shffl
-    global return_random
+
 #-------------------------------------#
 # V A R I A B L E S  T O  C O N T R O L
 #-------------------------------------#
-class return_njobs(object):
-#-------------------------------------#
-    def init():
-        return -1          #n_jobs is the number of parallel jobs
-#-------------------------------------#
-class return_xor():
-#-------------------------------------#
-    def init():
-        return 'accuracy'    #score is the parameter for scoring, such as 'precision'
-#-------------------------------------#
-def return_nneighbors():
-#-------------------------------------#
-    nneighbors = 5      #n_neighbors is the number of nearest neighbors to consider
-#-------------------------------------#
-def return_nsplits():
-#-------------------------------------#
-    nsplits = 50        #n_splits is number of splits for decisions
-#-------------------------------------#
-def return_nestimators():
-#-------------------------------------#
-    nestimators = 400   #n_estimators states how many estimators to allocate
-#-------------------------------------#
-class return_shffl(object):
-#-------------------------------------#
-    def init():
-        return True         #shuffle - refer to python.org documentation for details
-#-------------------------------------#
-def return_random():
-#-------------------------------------#
-    random = 42         #random_state - machine learning started seed.
-
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-# W O R K I N G  A R E A 
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+njobs = -1          #n_jobs is the number of parallel jobs
+xor = 'accuracy'    #score is the parameter for scoring, such as 'precision'
+nneighbors = 5      #n_neighbors is the number of nearest neighbors to consider
+nsplits = 10        #n_splits is number of splits for decisions
+nestimators = 400   #n_estimators states how many estimators to allocate
+shffl = True        #shuffle - refer to python.org documentation for details
+random = 42         #random_state - machine learning started seed.
 gama = "auto"       #deprecated variable added for SVM sectionto remove warnings.
-def refresh_variables():
-        njobs = return_njobs()         #n_jobs is the number of parallel jobs
-        xor = 'accuracy'    #score is the parameter for scoring, such as 'precision'
-        nneighbors = 5      #n_neighbors is the number of nearest neighbors to consider
-        nsplits = 50        #n_splits is number of splits for decisions
-        nestimators = 400   #n_estimators states how many estimators to allocate
-        shffl = True        #shuffle - refer to python.org documentation for details
-        random = 42          #random_state - machine learning started seed.
-        gama = "auto"       #deprecated variable added for SVM sectionto remove warnings.
+
+njobs_options = [1, -1]
+nneighbors_options = [1, 5, 10, 13, 15, 20]
+nsplits_options = [10, 20, 30, 40, 50, 100]
+nestimators_options = [10, 50, 100, 200, 400, 1000]
+shffl_options = [True, False]
+random_options = [0, 1, 2, 42]
 
 
 #-------------------------------------#
@@ -125,38 +86,62 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
 
-njobs_options = [1, -1]
-nneighbors_options = [1, 5, 10, 13, 15, 20]
-nsplits_options = [10, 20, 30, 40, 50, 100]
-nestimators_options = [10, 50, 100, 200, 400, 1000]
-shffl_options = [True, False]
-random_options = [0, 1, 2, 42]
-
-
 #--------------------------------------#
 # DEFINE K_FOLD
 #--------------------------------------#
-#k_fold = KFold(n_splits = return_nsplits(), shuffle = return_shffl(), random_state = return_random())
+k_fold = KFold(n_splits = nsplits, shuffle = shffl, random_state = random)
 
+#######################################
+# K  K   N     N  N     N
+# K K    N N   N  N N   N
+# K      N  N  N  N  N  N
+# K K    N   N N  N   N N
+# K  k   N    NN  N    NN
+#######################################
+#-------------------------------------#
+# KNN - vary shuffle
+#-------------------------------------#
 
-#-------------------------------------#
-# KNN - vary random_state
-#-------------------------------------#
-global_variables_function()
-refresh_variables()
 results = []
-for random in random_options:
-    clf = KNeighborsClassifier(n_neighbors = return_nneighbors())
-    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = 50, shuffle = return_shffl(), random_state = random, n_jobs = return_njobs(), scoring = return_xor()))
+for shffl_option in shffl_options:
+    k_fold = KFold(n_splits = nsplits, shuffle = shffl_option, random_state = random)
+    clf = KNeighborsClassifier(n_neighbors = nneighbors)
+    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl_option, random_state = random), n_jobs = njobs, scoring = xor)
     mean_score = round(np.mean(score) * 100, 2)
     results.append(mean_score)
     print("#-------------------------------------#")
-    print("KNN random using" ,random, "score:", end = '')
+    print("KNN shuffle using" ,shffl_option, "score:", end = '')
     print(score)
     print("#-------------------------------------#")
     print("")
     print("#-------------------------------------#")
-    print("# KNN random using",random, "MEAN score:", end = '')
+    print("# KNN shuffle using",shffl_option, "MEAN score:", end = '')
+    print(mean_score)
+    print("#-------------------------------------#")
+    print("")
+pd.Series(results, shffl_options).plot()
+plt.title('KNN Varying shuffle results')
+plt.xlabel('Shuffle')
+plt.ylabel('Accuracy')
+plt.show()
+#-------------------------------------#
+# KNN - vary random_state
+#-------------------------------------#
+
+results = []
+for random_option in random_options:
+    k_fold = KFold(n_splits = nsplits, shuffle = shffl, random_state = random_option)
+    clf = KNeighborsClassifier(n_neighbors = nneighbors)
+    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random_option), n_jobs = njobs, scoring = xor)
+    mean_score = round(np.mean(score) * 100, 2)
+    results.append(mean_score)
+    print("#-------------------------------------#")
+    print("KNN random using" ,random_option, "score:", end = '')
+    print(score)
+    print("#-------------------------------------#")
+    print("")
+    print("#-------------------------------------#")
+    print("# KNN random using",random_option, "MEAN score:", end = '')
     print(mean_score)
     print("#-------------------------------------#")
     print("")
@@ -168,22 +153,21 @@ plt.show()
 #-------------------------------------#
 # KNN - vary n_jobs
 #-------------------------------------#
-global_variables_function()
-refresh_variables()
+
 results = []
-for njob in njobs_options:
+for njob_option in njobs_options:
     k_fold = KFold(n_splits = nsplits, shuffle = shffl, random_state = random)
     clf = KNeighborsClassifier(n_neighbors = nneighbors)
-    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = njob, scoring = return_xor())
+    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = njob_option, scoring = xor)
     mean_score = round(np.mean(score) * 100, 2)
     results.append(mean_score)
     print("#-------------------------------------#")
-    print("KNN n_jobs using" ,njob, "score:", end = '')
+    print("KNN n_jobs using" ,njob_option, "score:", end = '')
     print(score)
     print("#-------------------------------------#")
     print("")
     print("#-------------------------------------#")
-    print("# KNN n_jobs using",njob, "MEAN score:", end = '')
+    print("# KNN n_jobs using",njob_option, "MEAN score:", end = '')
     print(mean_score)
     print("#-------------------------------------#")
     print("")
@@ -195,22 +179,21 @@ plt.show()
 #-------------------------------------#
 # KNN - vary n_splits
 #-------------------------------------#
-global_variables_function()
-refresh_variables()
+
 results = []
-for nsplit in nsplits_options:
-    k_fold = KFold(n_splits = nsplit, shuffle = shffl, random_state = random)
+for nsplit_option in nsplits_options:
+    k_fold = KFold(n_splits = nsplit_option, shuffle = shffl, random_state = random)
     clf = KNeighborsClassifier(n_neighbors = nneighbors)
-    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = return_njobs(), scoring = return_xor())
+    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplit_option, shuffle = shffl, random_state = random), n_jobs = njobs, scoring = xor)
     mean_score = round(np.mean(score) * 100, 2)
     results.append(mean_score)
     print("#-------------------------------------#")
-    print("KNN n_splits of" ,nsplit, "score:", end = '')
+    print("KNN n_splits of" ,nsplit_option, "score:", end = '')
     print(score)
     print("#-------------------------------------#")
     print("")
     print("#-------------------------------------#")
-    print("# KNN n_splits of",nsplit, "MEAN score:", end = '')
+    print("# KNN n_splits of",nsplit_option, "MEAN score:", end = '')
     print(mean_score)
     print("#-------------------------------------#")
     print("")
@@ -222,22 +205,21 @@ plt.show()
 #-------------------------------------#
 # KNN - vary n_neighbors
 #-------------------------------------#
-global_variables_function()
-refresh_variables()
+
 results = []
-for nneighbors in nneighbors_options:
-    k_fold = KFold(n_splits = nsplit, shuffle = shffl, random_state = random)
-    clf = KNeighborsClassifier(n_neighbors = nneighbors)
-    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = return_njobs(), scoring = return_xor())
+for nneighbors_option in nneighbors_options:
+    k_fold = KFold(n_splits = nsplits, shuffle = shffl, random_state = random)
+    clf = KNeighborsClassifier(n_neighbors = nneighbors_option)
+    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = njobs, scoring = xor)
     mean_score = round(np.mean(score) * 100, 2)
     results.append(mean_score)
     print("#-------------------------------------#")
-    print("KNN of" ,nneighbors, "score:", end = '')
+    print("KNN of" ,nneighbors_option, "score:", end = '')
     print(score)
     print("#-------------------------------------#")
     print("")
     print("#-------------------------------------#")
-    print("# KNN of",nneighbors, "MEAN score:", end = '')
+    print("# KNN of",nneighbors_option, "MEAN score:", end = '')
     print(mean_score)
     print("#-------------------------------------#")
     print("")
@@ -247,42 +229,12 @@ plt.xlabel('Number of n_neighbors')
 plt.ylabel('Accuracy')
 plt.show()
 
-#-------------------------------------#
-# KNN - vary shuffle
-#-------------------------------------#
-global_variables_function()
-refresh_variables()
-results = []
-for shffl in shffl_options:
-    k_fold = KFold(n_splits = nsplit, shuffle = shffl, random_state = random)
-    clf = KNeighborsClassifier(n_neighbors = nneighbors)
-    score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = return_njobs(), scoring = return_xor())
-    mean_score = round(np.mean(score) * 100, 2)
-    results.append(mean_score)
-    print("#-------------------------------------#")
-    print("KNN shuffle using" ,shffl, "score:", end = '')
-    print(score)
-    print("#-------------------------------------#")
-    print("")
-    print("#-------------------------------------#")
-    print("# KNN shuffle using",shffl, "MEAN score:", end = '')
-    print(mean_score)
-    print("#-------------------------------------#")
-    print("")
-pd.Series(results, shffl_options).plot()
-plt.title('KNN Varying shuffle results')
-plt.xlabel('Shuffle')
-plt.ylabel('Accuracy')
-plt.show()
-
-
-
 
 #
 ## Decision Tree
 #
 clf = DecisionTreeClassifier()
-score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = return_njobs(), scoring = return_xor())
+score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = njobs, scoring = xor)
 print("#-------------------------------------#")
 print("DecisionTree score: ", end = '')
 print(score)
@@ -305,7 +257,7 @@ print("")
 #
 
 clf = RandomForestClassifier(n_estimators = nestimators)
-score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = return_njobs(), scoring = return_xor())
+score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = njobs, scoring = xor)
 print("#-------------------------------------#")
 print("Random Forest Classifier score: ", end = '')
 print(score)
@@ -325,7 +277,7 @@ print("")
 #
 
 clf = GaussianNB()
-score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = return_njobs(), scoring = return_xor())
+score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = njobs, scoring = xor)
 print("#-------------------------------------#")
 print("Naive Bayes score: ", end = '')
 print(score)
@@ -343,7 +295,7 @@ print("")
 ## Support Vector Machine
 #
 clf = SVC(gamma = gama)
-score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = return_njobs(), scoring = return_xor())
+score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = njobs, scoring = xor)
 print("#-------------------------------------#")
 print("SVM score: ", end = '')
 print(score)
@@ -366,7 +318,7 @@ print("")
 #clf = KNeighborsClassifier(n_neighbors = 20)
 #clf = DecisionTreeClassifier()
 clf = RandomForestClassifier(n_estimators = nestimators)
-score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = return_njobs(), scoring = return_xor())
+score = cross_val_score(clf, training_data, target, cv = KFold(n_splits = nsplits, shuffle = shffl, random_state = random), n_jobs = njobs, scoring = xor)
 clf.fit(training_data, target)
 test_data = test.drop('passengerid', axis = 1).copy()
 prediction = clf.predict(test_data)
